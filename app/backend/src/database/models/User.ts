aeb8de081eba631cb7522139532f1395a8c324d7
Luiz Pereira <lfhbpereira@gmail.com>
2023-01-27 20:20:52 -0300

    feat(req01): add model to users table

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,40 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+export default class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: true,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'User',
+  timestamps: false,
+  underscored: true,
+});
