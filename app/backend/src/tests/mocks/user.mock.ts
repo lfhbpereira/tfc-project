ce9d8dfc83a2b04f6ce977526852eef77615c41d
Luiz Pereira <lfhbpereira@gmail.com>
2023-01-30 21:42:47 -0300

    test(req04): implement TDD

--- app/backend/src/tests/mocks/user.mock.ts
+++ app/backend/src/tests/mocks/user.mock.ts
@@ -2,3 +2,7 @@ export const login = {
   email: 'admin@admin.com',
   password: 'secret_admin',
 };
+
+export const loginWithoutEmail = {
+  password: 'secret_admin',
+};
--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -5,7 +5,7 @@ import * as sinon from 'sinon';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import { login } from '../tests/mocks/user.mock';
+import { login, loginWithoutEmail } from '../tests/mocks/user.mock';
 import User from '../database/models/user.model';
 
 const { expect } = chai;
@@ -13,6 +13,8 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('Test /login endpoint', () => {
+  afterEach(sinon.restore);
+
   describe('If a user tries to log in', () => {
     context('with right credentials', () => {
       it('should return the 200 status code with a token', async () => {
@@ -22,6 +24,17 @@ describe('Test /login endpoint', () => {
 
         expect(status).to.equal(200);
         expect(body).to.haveOwnProperty('token');
+      });
+    });
+
+    context('without providing email', () => {
+      it('should return the 400 status code with an error message', async () => {
+        sinon.stub(User, 'findOne').resolves(loginWithoutEmail as User);
+
+        const { status, body } = await chai.request(app).post('/login').send(loginWithoutEmail);
+
+        expect(status).to.equal(400);
+        expect(body.message).to.equal('All fields must be filled');
       });
     });
   });
