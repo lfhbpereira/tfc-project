b0594812688b832daf4d1609f86d7771771c7eb0
Luiz Pereira <lfhbpereira@gmail.com>
2023-02-02 16:39:44 -0300

    feat(req12): add GET route

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -1,15 +1,24 @@
 import * as express from 'express';
 
+import Token from '../auth/token';
 import UserController from '../controllers/user.controller';
 import ValidateUserLogin from '../middlewares/validateUserLogin';
 
 const userRouter = express.Router();
+
+const token = new Token();
 
 userRouter.post(
   '/',
   ValidateUserLogin.emailFieldValidation,
   ValidateUserLogin.passwordFieldValidation,
   UserController.login,
+);
+
+userRouter.get(
+  '/validate',
+  (req, res, next) => token.validate(req, res, next),
+  UserController.validate,
 );
 
 export default userRouter;
