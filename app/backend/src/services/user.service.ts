7eb0e96b2309d39ccc3dc052524474eca655b41c
Luiz Pereira <lfhbpereira@gmail.com>
2023-02-02 13:12:27 -0300

    feat(req11): add password verification

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,3 +1,5 @@
+import * as bcrypt from 'bcryptjs';
+
 import { IUserCredentials } from '../interfaces/IUser';
 import Token from '../auth/token';
 import UserModel from '../database/models/user.model';
@@ -6,11 +8,17 @@ const token = new Token();
 
 export default class UserService {
   static async login(login: IUserCredentials) {
-    const { email } = login;
+    const { email, password } = login;
 
     const user = await UserModel.findOne({ where: { email } });
 
     if (!user) {
+      return { type: 'Unauthorized', message: 'Incorrect email or password' };
+    }
+
+    const passcode = bcrypt.compareSync(password, user.password);
+
+    if (!passcode) {
       return { type: 'Unauthorized', message: 'Incorrect email or password' };
     }
 
