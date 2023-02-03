bd959be0280953c8d5888908eda422828fd39ca3
Luiz Pereira <lfhbpereira@gmail.com>
2023-02-03 01:04:58 -0300

    feat(req14): add model for teams table

---
+++ app/backend/src/database/models/team.model.ts
@@ -0,0 +1,25 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+export default class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  tableName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
