cd35b1adffbebb270d22003beb1c2811879eae0f
Luiz Pereira <lfhbpereira@gmail.com>
2023-02-04 02:05:50 -0300

    test(req17): implement TDD

---
+++ app/backend/src/tests/match.test.ts
@@ -0,0 +1,50 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import { finishedMatches, matches, matchesInProgress } from './mocks/match.mock';
+import MatchModel from '../database/models/match.model';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Test /matches endpoint', () => {
+  afterEach(sinon.restore);
+
+  describe('When searching for all matches', () => {
+    it('should return the 200 status code with all matches', async () => {
+      sinon.stub(MatchModel, 'findAll').resolves(matches as unknown as MatchModel[]);
+
+      const { status, body } = await chai.request(app).get('/matches');
+
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(matches);
+    });
+  });
+
+  describe('When searching for matches in progress', () => {
+    it('should return the 200 status code with the matches in progress', async () => {
+      sinon.stub(MatchModel, 'findAll').resolves(matchesInProgress as unknown as MatchModel[]);
+
+      const { status, body } = await chai.request(app).get('/matches?inProgress=true');
+
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(matchesInProgress);
+    });
+  });
+
+  describe('When searching for finished matches', () => {
+    it('should return the 200 status code with the finished matches', async () => {
+      sinon.stub(MatchModel, 'findAll').resolves(finishedMatches as unknown as MatchModel[]);
+
+      const { status, body } = await chai.request(app).get('/matches?inProgress=false');
+
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(finishedMatches);
+    });
+  });
+});
---
+++ app/backend/src/tests/mocks/match.mock.ts
@@ -0,0 +1,218 @@
+export const matches = [
+  {
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'São Paulo'
+    },
+    awayTeam: {
+      teamName: 'Grêmio'
+    },
+  },
+  {
+    id: 2,
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Internacional'
+    },
+    awayTeam: {
+      teamName: 'Santos'
+    },
+  },
+  {
+    id: 3,
+    homeTeamId: 4,
+    homeTeamGoals: 3,
+    awayTeamId: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Corinthians'
+    },
+    awayTeam: {
+      teamName: 'Napoli-SC'
+    },
+  },
+  {
+    id: 4,
+    homeTeamId: 3,
+    homeTeamGoals: 0,
+    awayTeamId: 2,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Botafogo'
+    },
+    awayTeam: {
+      teamName: 'Bahia'
+    },
+  },
+  {
+    id: 5,
+    homeTeamId: 7,
+    homeTeamGoals: 1,
+    awayTeamId: 10,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Flamengo'
+    },
+    awayTeam: {
+      teamName: 'Minas Brasília'
+    },
+  },
+];
+
+export const matchesInProgress = [
+  {
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 2,
+    awayTeamId: 9,
+    awayTeamGoals: 0,
+    inProgress: true,
+    homeTeam: {
+      teamName: 'São Paulo'
+    },
+    awayTeam: {
+      teamName: 'Internacional'
+    },
+  },
+  {
+    id: 2,
+    homeTeamId: 6,
+    homeTeamGoals: 1,
+    awayTeamId: 1,
+    awayTeamGoals: 0,
+    inProgress: true,
+    homeTeam: {
+      teamName: 'Ferroviária'
+    },
+    awayTeam: {
+      teamName: 'Avaí/Kindermann'
+    },
+  },
+  {
+    id: 3,
+    homeTeamId: 11,
+    homeTeamGoals: 0,
+    awayTeamId: 10,
+    awayTeamGoals: 0,
+    inProgress: true,
+    homeTeam: {
+      teamName: 'Napoli-SC'
+    },
+    awayTeam: {
+      teamName: 'Minas Brasília'
+    },
+  },
+  {
+    id: 4,
+    homeTeamId: 7,
+    homeTeamGoals: 2,
+    awayTeamId: 15,
+    awayTeamGoals: 2,
+    inProgress: true,
+    homeTeam: {
+      teamName: 'Flamengo'
+    },
+    awayTeam: {
+      teamName: 'São José-SP'
+    },
+  },
+  {
+    id: 5,
+    homeTeamId: 5,
+    homeTeamGoals: 1,
+    awayTeamId: 3,
+    awayTeamGoals: 1,
+    inProgress: true,
+    homeTeam: {
+      teamName: 'Cruzeiro'
+    },
+    awayTeam: {
+      teamName: 'Botafogo'
+    },
+  },
+];
+
+export const finishedMatches = [
+  {
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'São Paulo'
+    },
+    awayTeam: {
+      teamName: 'Grêmio'
+    },
+  },
+  {
+    id: 2,
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Internacional'
+    },
+    awayTeam: {
+      teamName: 'Santos'
+    },
+  },
+  {
+    id: 3,
+    homeTeamId: 4,
+    homeTeamGoals: 3,
+    awayTeamId: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Corinthians'
+    },
+    awayTeam: {
+      teamName: 'Napoli-SC'
+    },
+  },
+  {
+    id: 4,
+    homeTeamId: 3,
+    homeTeamGoals: 0,
+    awayTeamId: 2,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Botafogo'
+    },
+    awayTeam: {
+      teamName: 'Bahia'
+    },
+  },
+  {
+    id: 5,
+    homeTeamId: 7,
+    homeTeamGoals: 1,
+    awayTeamId: 10,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeTeam: {
+      teamName: 'Flamengo'
+    },
+    awayTeam: {
+      teamName: 'Minas Brasília'
+    },
+  },
+];
