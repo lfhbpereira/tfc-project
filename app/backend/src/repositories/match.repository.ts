c176ac1c6305cc972311266b10998380ed73c9ba
Luiz Pereira <lfhbpereira@gmail.com>
2023-02-10 00:45:49 -0300

    refactor(req19/20/21): rename match interface in layers

--- app/backend/src/repositories/match.repository.ts
+++ app/backend/src/repositories/match.repository.ts
@@ -1,11 +1,11 @@
-import IMatch from '../interfaces/IMatch';
+import { IMatches } from '../interfaces/IMatch';
 import IMatchRepository from './interfaces/IMatchRepository';
 import MatchModel from '../database/models/match.model';
 
 export default class MatchRepository implements IMatchRepository {
   constructor(private _matchModel: typeof MatchModel) {}
 
-  public async getAll(): Promise<IMatch[]> {
+  public async getAll(): Promise<IMatches[]> {
     const matches = await this._matchModel.findAll({
       include: [
         { association: 'homeTeam', attributes: ['teamName'] },
@@ -13,10 +13,10 @@ export default class MatchRepository implements IMatchRepository {
       ],
     });
 
-    return matches as unknown as IMatch[];
+    return matches as unknown as IMatches[];
   }
 
-  public async getByQuery(inProgress: boolean): Promise<IMatch[]> {
+  public async getByQuery(inProgress: boolean): Promise<IMatches[]> {
     const matches = await this._matchModel.findAll({ where: { inProgress },
       include: [
         { association: 'homeTeam', attributes: ['teamName'] },
@@ -24,6 +24,6 @@ export default class MatchRepository implements IMatchRepository {
       ],
     });
 
-    return matches as unknown as IMatch[];
+    return matches as unknown as IMatches[];
   }
 }
--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -1,17 +1,17 @@
-import IMatch from '../interfaces/IMatch';
+import { IMatches } from '../interfaces/IMatch';
 import IMatchRepository from '../repositories/interfaces/IMatchRepository';
 import IMatchService from './interfaces/IMatchService';
 
 export default class MatchService implements IMatchService {
   constructor(private _matchRepository: IMatchRepository) {}
 
-  public async getAll(): Promise<IMatch[]> {
+  public async getAll(): Promise<IMatches[]> {
     const matches = await this._matchRepository.getAll();
 
     return matches;
   }
 
-  public async getByQuery(inProgress: boolean): Promise<IMatch[]> {
+  public async getByQuery(inProgress: boolean): Promise<IMatches[]> {
     const matches = await this._matchRepository.getByQuery(inProgress);
 
     return matches;
