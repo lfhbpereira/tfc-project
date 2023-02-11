4572dbd9cd16a0e4a68c41c910f2fcd380afb2d8
Luiz Pereira <lfhbpereira@gmail.com>
2023-02-11 17:59:34 -0300

    test(req02): add user mock

--- app/backend/src/tests/mocks/user.mock.ts
+++ app/backend/src/tests/mocks/user.mock.ts
@@ -25,3 +25,11 @@ export const loginInvalidPassword = {
   email: 'admin@admin.com',
   password: 'secret_adminn',
 };
+
+export const user = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+}
