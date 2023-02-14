ca58f753bf8cdb837e45dcf6dd65270861a13dd6
Luiz Pereira <lfhbpereira@gmail.com>
2023-02-14 18:15:28 -0300

    feat(req29/30): add homeTeams query

---
+++ app/backend/src/utils/queries.ts
@@ -0,0 +1,22 @@
+const homeTeams = (
+  `SELECT t.team_name AS name,
+  SUM(if(m.home_team_goals > m.away_team_goals, 3,
+    if(m.home_team_goals = m.away_team_goals, 1, 0))) AS totalPoints,
+  COUNT(m.home_team_id) AS totalGames,
+  SUM(m.home_team_goals > m.away_team_goals) AS totalVictories,
+  SUM(m.home_team_goals = m.away_team_goals) AS totalDraws,
+  SUM(m.home_team_goals < m.away_team_goals) AS totalLosses,
+  SUM(m.home_team_goals) AS goalsFavor,
+  SUM(m.away_team_goals) AS goalsOwn,
+  SUM(m.home_team_goals - m.away_team_goals) AS goalsBalance,
+  round(SUM(if(m.home_team_goals > m.away_team_goals, 3,
+    if(m.home_team_goals = m.away_team_goals, 1, 0))) / (COUNT(m.id) * 3) * 100, 2) AS efficiency
+  FROM matches AS m
+  INNER JOIN teams AS t
+  ON m.home_team_id = t.id
+  WHERE m.in_progress = false
+  GROUP BY t.team_name
+  ORDER BY totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC, goalsOwn ASC`
+);
+
+export default homeTeams;
