faf8882fbcb9ec4dd2c1947d19f8c97436fccf46
Luiz Pereira <lfhbpereira@gmail.com>
2023-02-14 19:00:57 -0300

    feat(req29/30): add route for home teams in leaderboard router

---
+++ app/backend/src/routers/leaderboard.router.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import LeaderboardController from '../controllers/leaderboard.controller';
+import LeaderboardRepository from '../repositories/leaderboard.repository';
+import LeaderboardService from '../services/leaderboard.service';
+
+export default class LeadeboardRouter {
+  public router: Router;
+  private _leaderboardRepository: LeaderboardRepository;
+  private _leaderboardService: LeaderboardService;
+  private _leaderboardController: LeaderboardController;
+
+  constructor() {
+    this.router = Router();
+    this._leaderboardRepository = new LeaderboardRepository();
+    this._leaderboardService = new LeaderboardService(this._leaderboardRepository);
+    this._leaderboardController = new LeaderboardController(this._leaderboardService);
+
+    this.router.get('/home', this._leaderboardController.getHomeTeamLeaderboard);
+  }
+}
